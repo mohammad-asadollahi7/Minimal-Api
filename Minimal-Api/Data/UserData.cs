a13c3b193a3af533b529890a1581ba62298a4c0b
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-07-27 18:51:25 +0430

    Add some data and dataAccess

---
+++ Minimal-Api/Data/UserData.cs
@@ -0,0 +1,32 @@
+﻿using Minimal_Api.Model;
+using System.Text.Json;
+
+namespace Minimal_Api.Data;
+
+public class UserData
+{
+    private string FilePath;
+    List<User>? users;
+    public UserData()
+    {
+        FilePath = GetFilePath();
+        var jsonString = File.ReadAllText(FilePath);
+        users = JsonSerializer.Deserialize<List<User>>(jsonString);
+    }
+
+    public void Savechanges()
+    {
+        var jsonString = JsonSerializer.Serialize(users);
+        File.WriteAllText(FilePath, jsonString);
+    }
+
+    private string GetFilePath()
+    {
+        string workingDirectory = Environment.CurrentDirectory;
+        string? projectDirectory = Directory.GetParent(workingDirectory)?
+                                    .Parent?.Parent?.FullName;
+        return Path.Combine(projectDirectory, "Data", "User.json");
+    }
+
+
+}
