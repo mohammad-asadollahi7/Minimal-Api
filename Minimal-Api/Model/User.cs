11dd9054763dd018680d05850b4cfba12c4dc08f
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-07-27 18:24:01 +0430

    Add user model

---
+++ Minimal-Api/Model/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Minimal_Api.Model;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Family { get; set; }
+    public string Email { get; set; }
+}
